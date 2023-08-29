1dc8f8657415f6352238d639b4abb8e32b23bf24
yumion <a.yamada0703@gmail.com>
2023-08-29 10:21:45 +0900

    update Dockerfile

---
+++ docker/run.sh
@@ -0,0 +1,15 @@
+CONTAINER_NAME=$1
+
+cd /data2/src/atsushi/mmpretrain
+docker run \
+    --gpus all \
+    --shm-size=64gb \
+    --restart unless-stopped\
+    -v /data1:/data1 \
+    -v /data2:/data2 \
+    -itd \
+    --name $CONTAINER_NAME \
+    mmpretrain:pytorch1.12.1-cuda11.3-cudnn8-mmcv2.0.1-mmengine0.8.4
+
+docker exec -it $CONTAINER_NAME bash -c \
+    "cd $PWD && pip install --no-cache-dir -e . && pip install -r requirements/optional.txt"
